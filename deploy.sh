9f8717e61cc0f0ae32f230535713ab3fe10f1bb1
Jameo35 <james.hirst19@gmail.com>
2022-05-27 11:54:52 +0100

    Pull current build

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-DOCKERHUB_USER=jameo35
-APP_NAME=lbg-api-app
-
-#build image
-docker rmi -f ${DOCKERHUB_USER}/${APP_NAME}
-docker build -t ${DOCKERHUB_USER}/${APP_NAME} .
-#push to docker hub
-docker push ${DOCKERHUB_USER}/${APP_NAME}
-
-# run container from image
-docker rm -f ${APP_NAME}
-docker run  -d -p 80:4000 --name ${APP_NAME} ${DOCKERHUB_USER}/${APP_NAME}
-
+kubectl apply -f kubernetes
+kubectl get pods
+kubectl get deployments
+kubectl get svc
 docker system prune -f
